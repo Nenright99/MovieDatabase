625d31667c02e0791d8a914ae2b2936cd126b839
Nicholas Enright <NEnright1999@gmail.com>
2021-07-22 10:58:19 -0400

    Entities added with save

--- MovieDatabase.Data/Entities/Review.cs
+++ MovieDatabase.Data/Entities/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,13 @@ namespace MovieDatabase.Data.Entities
 {
     public class Review
     {
+        [Key]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorID { get; set; }
+        public DateTimeOffset CreatedUTC { get; set; }
+        [ForeignKey(nameof(Movie))]
+        public int RevID { get; set; }
+        public virtual Movie Movie { get; set; }
     }
 }
